64894579f02d2b1ae5fbb541e1092e1c797a80f1
Tarun Menta <menta.tarun@gmail.com>
2025-06-17 00:49:03 -0400

    Handle list indentation in continuations better

--- marker/processors/list.py
+++ marker/processors/list.py
@@ -1,9 +1,11 @@
+from collections import defaultdict
 from typing import Annotated, List, Tuple
 
 from marker.processors import BaseProcessor
 from marker.schema import BlockTypes
 from marker.schema.blocks import ListItem
 from marker.schema.document import Document
+from marker.schema.polygon import PolygonBox
 
 
 class ListProcessor(BaseProcessor):
@@ -57,10 +59,11 @@ class ListProcessor(BaseProcessor):
     def list_group_indentation(self, document: Document):
         for page in document.pages:
             for block in page.contained_blocks(document, self.block_types):
-                if block.structure is None:
+                if block.structure is None or block.ignore_for_output:
                     continue
-                if block.ignore_for_output:
-                    continue
+
+                indent_xstarts = defaultdict(list)
+                max_indent_level = 0
 
                 stack: List[ListItem] = [block.get_next_block(page, None)]
                 for list_item_id in block.structure:
@@ -77,6 +80,10 @@ class ListProcessor(BaseProcessor):
                         list_item_block.list_indent_level = stack[-1].list_indent_level
                         if list_item_block.polygon.x_start > stack[-1].polygon.x_start + (self.min_x_indent * page.polygon.width):
                             list_item_block.list_indent_level += 1
+
+                    # Track indent x_start by level
+                    indent_xstarts[list_item_block.list_indent_level].append(list_item_block.polygon.x_start)
+                    max_indent_level = max(max_indent_level, list_item_block.list_indent_level)
 
                     next_list_item_block = block.get_next_block(page, list_item_block)
                     if next_list_item_block is not None and next_list_item_block.polygon.x_start > list_item_block.polygon.x_end:
@@ -98,3 +105,28 @@ class ListProcessor(BaseProcessor):
 
                         block.remove_structure_items([list_item_id])
                     stack.append(list_item_block)
+
+                # Inject dummy blocks if current block has continuation
+                if getattr(block, "has_continuation", False):
+                    # This is guaranteed to be a listgroup due to how the flag is set, double checking anyways
+                    next_block = document.get_next_block(block, self.ignored_block_types)
+                    if next_block is None or next_block.block_type not in self.block_types:
+                        continue
+
+                    next_block_page = document.get_page(next_block.page_id)
+                    next_block_x_start = next_block.polygon.x_start
+                    next_block_x_end = next_block.polygon.x_end
+                    next_block_y_start = next_block.polygon.y_start
+                    for level in range(max_indent_level + 1):
+                        rel_x_start = min(indent_xstarts[level]) - block.polygon.x_start - 3
+
+                        polygon = PolygonBox.from_bbox([
+                            next_block_x_start + rel_x_start,
+                            next_block_y_start - (10 * (level + 1)),
+                            next_block_x_end,
+                            next_block_y_start - (5 * (level + 1))
+                        ])
+                        dummy_block = next_block_page.add_block(ListItem, polygon)
+                        next_block.structure.insert(level, dummy_block.id)
+                        # Let the next iteration re-calculate. This is the default
+                        dummy_block.list_indent_level = 0
